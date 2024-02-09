eca5fd090eb1656822c35c3c9585a2ee304572d2
TalkingCoffeePot <barrberry666@gmail.com>
2024-02-09 16:22:39 +0500

    On branch main
     Changes to be committed:
    	modified:   api_v1/views.py
        Добавлен кастомный класс IsAllowed
        с разными разрешениями

--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -3,15 +3,38 @@ from django.shortcuts import render
 from api_v1.serializers import PostModelSerializer
 from feed.models import PostModel
 from django.http import JsonResponse
+from rest_framework.permissions import BasePermission
+
+class IsAllowed(BasePermission):
+
+    def find_post(self, id):
+        return PostModel.objects.get(id=id)
+
+    def url_parse(self, str):
+        return int(str.split('/')[3])
+
+    def has_permission(self, request, view):
+        if request.user and request.user.is_authenticated:
+            if view.action == 'partial_update' or view.action == 'destroy':
+                if self.find_post(self.url_parse(request.path)) in request.user.usr_posts.all():
+                    return True
+                else: return False
+            else: return True
+        elif view.action == 'list' or view.action == 'retrieve':
+            return True
+        else: return False
 
 
 class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAllowed]
     queryset = PostModel.objects.all()
     serializer_class = PostModelSerializer
 
+    def current_object(self):
+        return self.get_object()
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
 
 
 def post_like_view(request):
